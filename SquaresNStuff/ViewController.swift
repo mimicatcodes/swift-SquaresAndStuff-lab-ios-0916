6f319b95fad74c463fa4acdef74ddb99d43891da
Mirim An <mimicat@Mirims-MacBook-Air.local>
2016-10-13 11:46:04 -0400

    Done

--- SquaresNStuff/ViewController.swift
+++ SquaresNStuff/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileImageView.layer.borderColor = UIColor.white.cgColor
+        profileImageView.layer.borderWidth = 5.0
+        profileImageView.layer.cornerRadius = 10.0
+        profileImageView.layer.masksToBounds = true
     }
+    
 }
+
+
